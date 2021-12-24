1575e48824141a23883b08bcab4bc811f1d50904
MAFIA <mosaabmograbe@yahoo.com>
2021-12-24 16:45:30 +0200

    finsh script

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('Testing');
+const videoElement = document.getElementById('video');
+const picButton = document.getElementById('picBtn');
+const shareButton = document.getElementById('shareBtn');
+// prompt to select medie stream, pass to video element,then play
+async function selectMediaStream() {
+    try{
+           const mediastream = await navigator.mediaDevices.getDisplayMedia();
+           videoElement.srcObject = mediastream;
+           videoElement.onloadedmetadata = () =>{
+               videoElement.play();
+           }
+           console.log(mediastream);
+    }catch(error){
+      console.log('whoops! error here :',error);
+    }
+}
+picButton.addEventListener('click', async() =>{
+    //disable button 
+    picButton.disabled = true;
+    //start pic in pic 
+    await videoElement.requestPictureInPicture();
+    //reset button
+    picButton.disabled = false; 
+});
+shareButton.addEventListener('click',selectMediaStream);
+//on load
+//selectMediaStream();
